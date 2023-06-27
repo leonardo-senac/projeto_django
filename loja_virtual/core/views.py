99dcdd3b147a88b9f9783bd603c57f72439d4a82
leonardo-senac <137335225+leonardo-senac@users.noreply.github.com>
2023-06-27 16:49:27 -0300

    Alterações em sala

--- loja_virtual/core/views.py
+++ loja_virtual/core/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import *
+from .models import * # Tradução: Do arquivo models, traga tudo (*)
 
 # Create your views here.
 
-def lista_clientes(request):
-    clientes = Cliente.objects.all()
+def lista_clientes(request): #View para mandar os clientes para o html
+    clientes = Cliente.objects.all() # Puxa todos os clientes do banco de dados e salva na lista
+                                     # chamada clientes
+    #Renderizando o clientes.html passando a lista de cliente para ser acessada pelo django
     return render(request, 'clientes.html', {'clientes': clientes})
 
-def cadastrar_cliente_endereco(request):
+def cadastrar_cliente_endereco(request): # View para receber os dados do cliente e cadastrar no banco de dados
     # Puxando os dados do endereço e cadastrando ele
     nova_rua = request.POST.get('rua')
     novo_bairro = request.POST.get('bairro')
@@ -30,12 +32,12 @@ def cadastrar_cliente_endereco(request):
     return redirect(lista_clientes)
 
 
-def lista_produtos(request):
+def lista_produtos(request): # View para puxar os produtos do banco e mandar para o html
     produtos = Produtos.objects.all()
     categorias = Categoria.objects.all()
     return render(request, 'produtos.html', {'produtos': produtos, 'categorias': categorias})
 
-def cadastrar_produto(request):
+def cadastrar_produto(request): # View para receber o produto do html e cadastrar no banco
     novo_nome = request.POST.get('nome_produto')
     nova_descricao = request.POST.get('descricao')
     novo_preco = request.POST.get('preco')
@@ -47,7 +49,7 @@ def cadastrar_produto(request):
 
     return redirect(lista_produtos)
 
-def cadastrar_categoria(request):
+def cadastrar_categoria(request): # View para receber a categoria do html e cadastrar no banco
     novo_nome = request.POST.get('nome_categoria')
 
     Categoria.objects.create(nome=novo_nome)
